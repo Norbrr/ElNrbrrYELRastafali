cdd3a12fa91993c608c9a12d1cd1346f1b60e4b7
Norbrr <norber@uma.es>
2025-07-18 14:14:28 +0200

    Inicio del proyecto (Se vienen Cositas)
    A

---
+++ Backend/Proyecto_Elfi_Stone/src/main/java/ElNrbrrYRastafali/App.java
@@ -0,0 +1,13 @@
+package ElNrbrrYRastafali;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
